d4578ee18f9ac55cf206d5b6a3a26d23cc02dcb5
pavlo-onyshchak <36053219+pavlo-onyshchak@users.noreply.github.com>
2018-03-10 19:01:14 +0200

    Update main.cpp

--- galera/Project1/main.cpp
+++ galera/Project1/main.cpp
@@ -9,16 +9,7 @@ using namespace std;
 
 int main()
 {
-	employee first;
-	first.setName("Bozhena");
-	first.setSurname("Gamada");
-	first.setSallary(300);
-	string k = first.getName();
-	string x = first.getSurname();
-	int r = first.getSallary();
-	cout << "the name of employee is -" << k << endl;
-	cout << "the surname of employee is -" << x << endl;
-	cout << "the sallary of employee is -" << r << endl;
+	
    system("pause");
 	return 0;
 }
