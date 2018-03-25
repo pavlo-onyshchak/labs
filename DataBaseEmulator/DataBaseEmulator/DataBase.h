173109aeb007c03fb55cbc268e47583666cec37b
Oleh Onyshchak <oleh.onyshchak@gmail.com>
2018-03-25 15:34:20 +0300

    Naming changes

--- DataBaseEmulator/DataBaseEmulator/DataBase.h
+++ DataBaseEmulator/DataBaseEmulator/DataBase.h
@@ -13,16 +13,16 @@ public:
 	friend ostream& operator<<(ostream& out, const DataBase&); // 1.1, 2.2
 
 	bool find(const string& id, ItCompany&) const; // 1.2
-	bool find(const vector<string>& ids, vector<ItCompany>&) const; // 2.3
+	bool find(const vector<string>& idCollection, vector<ItCompany>&) const; // 2.3
 
 	bool remove(const string& id); // 1.3
-	bool remove(const vector<string>& ids); // 2.4
+	bool remove(const vector<string>& idCollection); // 2.4
 
 	bool insert(const string& id); // 1.4
-	bool insert(const vector<string>& ids); // 2.5
+	bool insert(const vector<string>& idCollection); // 2.5
 
 	bool update(const string& id, const ItCompany&); // 1.5
-	bool update(const vector<string>& ids, const vector<ItCompany>&); // 2.6
+	bool update(const vector<string>& idCollection, const vector<ItCompany>&); // 2.6
 private:
 	fstream _file;
 };
